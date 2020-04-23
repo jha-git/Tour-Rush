e3fbd6d3b98716a4118e673f01d6473cb617135b
Workspace <gobind.jha@yahoo.com>
2020-04-23 19:32:38 +0530

    Static Webpage

---
+++ precache-manifest.14dd17197c8447247f3b19eb6bcd4f19.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d79517b7bf14e2de7daf1c060a6104",
+    "url": "/Tour-Rush/index.html"
+  },
+  {
+    "revision": "edb8e1d96436c9743408",
+    "url": "/Tour-Rush/static/css/main.12ca103a.chunk.css"
+  },
+  {
+    "revision": "3ef7d5d3d9c2c331fa3c",
+    "url": "/Tour-Rush/static/js/2.654fc7ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tour-Rush/static/js/2.654fc7ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb8e1d96436c9743408",
+    "url": "/Tour-Rush/static/js/main.d96063e1.chunk.js"
+  },
+  {
+    "revision": "3b1851bfa4ef34097d10",
+    "url": "/Tour-Rush/static/js/runtime-main.c7b86563.js"
+  },
+  {
+    "revision": "e994ad6d87be98d663d751312342e124",
+    "url": "/Tour-Rush/static/media/logo-white.e994ad6d.png"
+  },
+  {
+    "revision": "7fdf2a886516e020e300ae6bd544ab7d",
+    "url": "/Tour-Rush/static/media/nat-1-large.7fdf2a88.jpg"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/Tour-Rush/static/media/nat-10.2797561f.jpg"
+  },
+  {
+    "revision": "7a25f1999a15760508f5e6131245aeac",
+    "url": "/Tour-Rush/static/media/nat-2-large.7a25f199.jpg"
+  },
+  {
+    "revision": "417d3d741a7a47b3d6ef0307228033a0",
+    "url": "/Tour-Rush/static/media/nat-3-large.417d3d74.jpg"
+  },
+  {
+    "revision": "61bbf12ae90a30cd432d77087921a02f",
+    "url": "/Tour-Rush/static/media/nat-4.61bbf12a.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/Tour-Rush/static/media/nat-5.19f9a22f.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/Tour-Rush/static/media/nat-6.1ef6d764.jpg"
+  },
+  {
+    "revision": "5f846d16e6be9a73e6eb73f620379af2",
+    "url": "/Tour-Rush/static/media/nat-7.5f846d16.jpg"
+  },
+  {
+    "revision": "14efd82b81787a9764b2cb7eb7afd3e8",
+    "url": "/Tour-Rush/static/media/nat-8.14efd82b.jpg"
+  },
+  {
+    "revision": "b525a99bcf20d08b2aaf6dd83b7be623",
+    "url": "/Tour-Rush/static/media/nat-9.b525a99b.jpg"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/Tour-Rush/static/media/video.ffcd876c.mp4"
+  }
+]);
